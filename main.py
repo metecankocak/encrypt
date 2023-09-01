98d07e879a611bf48d0d6fffc8c16242f4a6f4c9
Mete Can Koçak <metecan05@gmail.com>
2023-09-01 13:51:18 +0300

    dic. name and textbox added

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
-
-
 window = Tk()
 
 window.title("IDK")
@@ -12,6 +10,19 @@ window.minsize(350,700)
 img = ImageTk.PhotoImage(Image.open("topsecret.png"))
 panel = Label(window, image = img, height=220, width=200)
 panel.pack(fill = "both")
+
+title_label= Label(text="Lütfen dosyanızın adını yazın.",)
+title_label.pack()
+title_entry = Entry(width=30)
+title_entry.pack()
+
+text_label= Label(text="Lütfen şifrelemek istediğiniz metni yazın.")
+text_label.pack()
+my_text= Text(height=10, width=40)
+my_text.pack()
+
+
+
 
 
 
