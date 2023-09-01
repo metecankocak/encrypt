2328763426af385955fd9ebd4eac3f0e0cdc589c
Mete Can Koçak <metecan05@gmail.com>
2023-09-01 14:33:10 +0300

    gerekli dosyalar da eklendi

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import tkinter.messagebox
 from tkinter import *
 from PIL import ImageTk, Image
 
@@ -15,28 +16,36 @@ title_label= Label(text="Lütfen dosyanızın adını yazın.",)
 title_label.pack()
 title_entry = Entry(width=30)
 title_entry.pack()
+title_entry.get()
+title_entry.focus()
 
 text_label= Label(text="Lütfen şifrelemek istediğiniz metni yazın.")
 text_label.pack()
 my_text= Text(height=15, width=45)
 my_text.pack()
+my_text.get("1.0", END)
 
 password_label= Label(text= "Lütfen şifrenizi giriniz.")
 
 password_label.pack()
 password_entry = Entry(width=30, )
 password_entry.pack()
+password_entry.get()
 
-save_button = Button(text="Kaydet ve şifrele")
+def click_save():
+    pass
+
+def click_decrypt():
+    pass
+
+
+save_button = Button(text="Kaydet ve şifrele", command=click_save)
 save_button.pack()
 
 decrypt_button=Button(text="şifreyi çöz")
 decrypt_button.pack()
 
 my_text.pack()
-
-
-
 
 
 
